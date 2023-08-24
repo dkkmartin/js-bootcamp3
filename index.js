8d070f2a64bca9672a5dbf463e56a1c7f1fe985f
Martin <martinbruun1995@gmail.com>
2023-08-24 13:09:57 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const main = document.querySelector('main')
+const link1 = document.querySelector('#link1')
+const link2 = document.querySelector('#link2')
+const link3 = document.querySelector('#link3')
+
+link1.addEventListener('click', () => {
+
+})
+
+link2.addEventListener('click', () => {
+  
+})
+
+link3.addEventListener('click', () => {
+  
+})
